b02c7d70b16a6776c97f83ac81bf370e8dd8e037
ovaphlow <jsfreecss@163.com>
2017-09-18 08:51:36 +0800

    .

--- public/index.js
+++ public/index.js
@@ -3,7 +3,6 @@ var appModule = angular.module('app', ['ngRoute'])
 appModule.controller('indexCtrl', function ($scope, $http) {
 
     $scope.search = function () {
-        // alert($scope.keyword)
         $http({
             url: '/search',
             method: 'POST',
--- router/stats.router.js
+++ router/stats.router.js
@@ -3,18 +3,20 @@ const logger = require('tracer').colorConsole()
 const sequelize = require('../util/sequelize')
 
 const stats = {
+
     search: (ctx) => {
         let sql = `
             select id, name, address, address1, address2
             from hola_place
             where locate(:keyword, name) > 0
                 or locate(:keyword, intro) > 0`
+        let result
         sequelize.query(sql, {
             replacements: ctx.request.body,
             type: sequelize.QueryTypes.SELECT
         }).then(function (data) {
             logger.trace(data)
-            // ctx.body = data
+            result = data
             ctx.body = {message: 'OK'}
         })
     }
